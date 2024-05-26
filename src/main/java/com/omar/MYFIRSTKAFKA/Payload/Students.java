3148fee259c67622ea8e9a517c78d8ef3cf3631d
OMAR ABARRA <abarraomar@gmail.com>
2024-05-26 18:37:40 +0100

    26 may

--- src/main/java/com/omar/MYFIRSTKAFKA/Payload/Students.java
+++ src/main/java/com/omar/MYFIRSTKAFKA/Payload/Students.java
@@ -3,9 +3,11 @@ package com.omar.MYFIRSTKAFKA.Payload;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Students {
 
 	private int id;
